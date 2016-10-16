8264223fac0d057e1abb576f546c7f2a39c20c3f
Jamnie <jamnie@126.com>
2016-10-16 19:04:02 +0800

    v7

--- src/com/atguigu/dao/UserDaoImpl.java
+++ src/com/atguigu/dao/UserDaoImpl.java
@@ -16,6 +16,7 @@ public class UserDaoImpl {
 			logger.debug("gitlianxi33");
 			logger.debug("gitlianxi44");
 			logger.debug("gitlianxi55");
+			logger.debug("gitlianxi77");
 		}
 
 		if (logger.isDebugEnabled()) {
