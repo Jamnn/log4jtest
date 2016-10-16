fea3d3f217d4b4f3765ef54c824b3eb392fc2240
Jamnie <jamnie@126.com>
2016-10-16 17:53:16 +0800

    新建项目

---
+++ src/com/atguigu/dao/UserDaoImpl.java
@@ -0,0 +1,44 @@
+package com.atguigu.dao;
+
+import org.apache.log4j.Logger;
+
+public class UserDaoImpl {
+	
+	//第一行永远是他
+	private static final Logger logger = Logger.getLogger(UserDaoImpl.class);
+
+	
+	public void getUserInfo(){
+		if (logger.isDebugEnabled()) {
+			logger.debug("getUserInfo() - start");
+		}
+
+		if (logger.isDebugEnabled()) {
+			logger.debug("getUserInfo() - end");
+		}
+	}
+	
+	public static void main(String[] args){
+		
+		logger.debug("-------debug");
+		logger.info("---------info");
+		logger.warn("---------warn");
+		logger.error("--------error");
+		
+		try {
+			
+			logger.debug("-----age come here");			
+			int age = 10/2;
+			if (logger.isInfoEnabled()) {
+				logger.info("main(String[]) - int age=" + age);
+			}
+			
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+			logger.error(e.getMessage(),e);
+		}
+		
+	}
+	
+}
